b2daa1f6f609f6f20a7ffcf2b81c9f03be6f6648
HudaSale7 <hudasaleh640@gmail.com>
2023-06-19 19:46:19 +0300

    update

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -43,7 +43,6 @@ exports.signup = async (req, res, next) => {
 }
 
 exports.login = async (req, res, next) => {
-    console.log(req.body);
     const userName = req.body.userName;
     const password = req.body.password;
     try {
